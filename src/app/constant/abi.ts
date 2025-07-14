ec11da191b96dcc0b48065255a51f0f6281721a0
jayakumar <146693424+JayaKumar200@users.noreply.github.com>
2025-07-14 17:11:05 +0530

    Update abi.ts

--- src/app/constant/abi.ts
+++ src/app/constant/abi.ts
@@ -1,23 +1,3 @@
-// export const contractAddress = "0xefe3ab3eff23bdff0e2f4cb910cdd88813a2234c"
-// export const contractABI = [
-//   {
-//     inputs: [{ internalType: "string", name: "newMessage", type: "string" }],
-//     name: "setMessage",
-//     outputs: [],
-//     stateMutability: "nonpayable",
-//     type: "function"
-//   },
-//   {
-//     inputs:[],
-//     name:"getMessage",
-//     outputs:[{ internalType:"string",name:"readMessage", type:"string"}],
-//     stateMutability:"view",
-//     type: "function"
-//   }
-// ];
-
-
-
 export const contractAddress = "0xefe3ab3eff23bdff0e2f4cb910cdd88813a2234c"
 export const contractABI = [
   {
